ce4c4cf3811750392701763eebe96311a836ba49
Cuong Van <cuongvtien@gmail.com>
2020-03-18 01:08:36 +0700

    get call param file

--- src/main/java/httpserver/endpoints/AppCallEndpoint.java
+++ src/main/java/httpserver/endpoints/AppCallEndpoint.java
@@ -2,9 +2,14 @@ package httpserver.endpoints;
 
 import externalapi.appcall.models.CallDetail;
 import externalapi.appcall.AppCallDAO;
-import externalapi.appinfo.models.AppInfo;
+import externalapi.appcall.models.CallParam;
+import externalapi.appcall.models.FileParam;
 import httpserver.common.BasicResponse;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
@@ -40,6 +45,56 @@ public class AppCallEndpoint {
             return Response.ok(callDetail).build();
         } else {
             return Response.status(Response.Status.NOT_FOUND).entity(BasicResponse.fail("App call not found")).build();
+        }
+    }
+    
+    @Path("/{callId}/{fileParamName}")
+    @GET
+    public Response getCodeFile(@PathParam("callId") String callId, @PathParam("fileParamName") String fileParamName) {
+        CallDetail callDetail = appCallDAO.getById(callId);
+        
+        if (callDetail == null) {
+            return Response
+                .status(Response.Status.NOT_FOUND)
+                .entity(BasicResponse.fail("App call not found"))
+                .type("application/json")
+                .build();
+        }
+        
+        Optional<CallParam> paramOpt = callDetail.getParams().stream()
+            .filter(p -> p.getName().equals(fileParamName))
+            .findFirst();
+        
+        if (!paramOpt.isPresent()) {
+            return Response
+                .status(Response.Status.NOT_FOUND)
+                .type("application/json")
+                .entity(BasicResponse.fail("Param not found"))
+                .build();
+        }
+        
+        CallParam param = paramOpt.get();
+        if (!(param instanceof FileParam)) {
+            return Response
+                .status(Response.Status.BAD_REQUEST)
+                .type("application/json")
+                .entity(BasicResponse.fail("Not a file param"))
+                .build();
+        }
+        
+        String filePath = ((FileParam) param).getFilePath();
+        try {
+            byte[] file = Files.readAllBytes(Paths.get(filePath));
+            return Response
+                .ok(file)
+                .type("application/octet-stream")
+                .build();
+        } catch (IOException ex) {
+            return Response
+                .status(Response.Status.INTERNAL_SERVER_ERROR)
+                .type("application/json")
+                .entity(BasicResponse.fail("Input file not found. It may have been deleted"))
+                .build();
         }
     }
 }
---
+++ test_scripts/get_call_param_file.sh
@@ -0,0 +1,2 @@
+CALL_ID=e04ff14b-6871-11ea-99d6-f76cd41399fd
+http :5001/call/$CALL_ID/file
