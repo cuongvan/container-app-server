d9d5387a2bc4bb870216fada02b1599c7186f049
Cuong Van <cuongvtien@gmail.com>
2020-03-19 23:49:52 +0700

    remove all tests

--- src/test/java/helpers/TestConstants.java
+++
@@ -1,9 +0,0 @@
-package helpers;
-
-import common.Constants;
-
-public class TestConstants {
-    public static final String BASE_URI = "http://localhost";
-    public static final int PORT = Constants.SERVER_PORT;
-    public static final String BASE_URI_PORT = "http://localhost:" + PORT;
-}
