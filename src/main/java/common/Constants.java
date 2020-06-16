d851ef9e9d718b67bc7bb8e3075224b46126c8e1
Cuong Van <cuongvtien@gmail.com>
2020-06-16 22:16:41 +0700

    add ping endpoint for ckan app to check

--- src/main/java/common/Constants.java
+++ src/main/java/common/Constants.java
@@ -1,7 +1,7 @@
 package common;
 
 public class Constants {
-    public static final String CONTAINER_INPUT_FILES_MOUNT_DIR = "/files";
+    public static final String CONTAINER_INPUT_FILES_MOUNT_DIR = "/input-files";
     
     public static final String CONTAINER_OUTPUT_FILES_DIR = "/outputs";
     public static final String CONTAINER_OUTPUT_FIELDS_FILE_RELATIVE_PATH = "output.json"; // /outputs/output.json
